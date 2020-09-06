3147db1290de43d356380737a1a7a7b69f450b41
Hugo Goes <hugo.henkel@hotmail.com>
2020-09-06 18:25:43 -0300

    estrutura inicial

---
+++ src/main/java/com/project/cadastrodeservicos/CadastrodeservicosApplication.java
@@ -0,0 +1,13 @@
+package com.project.cadastrodeservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrodeservicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrodeservicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/cadastrodeservicos/CadastrodeservicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cadastrodeservicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrodeservicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
